fa01da62c8c7fc097a07896a88315bb0aacde7c8
JayPerez1 <jay.perez1@gmail.com>
2021-03-08 16:51:53 -0500

    Added UUID 03-08-2021

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,8 +135,8 @@
 #define CUSTOM_MACHINE_NAME "6040 03-08-2021"   //BluJay
 
 // Printer's unique ID, used by some programs to differentiate between machines.
-// Choose your own or use a service li25ke https://www.uuidgenerator.net/version4
-//#define MACHINE_UUID "00000000-0000-0000-0000-000000000000"
+// Choose your own or use a service like https://www.uuidgenerator.net/version4
+//#define MACHINE_UUID "55bce20e-8058-11eb-9439-0242ac130002"
 
 // @section extruder
 
